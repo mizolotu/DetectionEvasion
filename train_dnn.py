ec883abf2ef38e073a8d5678131aa5945a6b406c
mizolotu <38492363+mizolotu@users.noreply.github.com>
2020-02-17 10:45:56 +0200

    17.12.2019

--- train_dnn.py
+++ train_dnn.py
@@ -1,5 +1,16 @@
+import tensorflow as tf
 import numpy as np
+
 from data_proc import load_dataset
+
+def create_model(nfeatures, nlayers, nhidden, ncategories, batch=512):
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Input(shape=(nfeatures,)))
+    for _ in range(nlayers):
+        model.add(tf.keras.layers.Dense(nhidden, activation='relu'))
+    model.add(tf.keras.layers.Dense(ncategories, activation='softmax'))
+    model.compile(loss='categorical_crossentropy')
+    return model
 
 if __name__ == '__main__':
 
@@ -7,11 +18,12 @@ if __name__ == '__main__':
 
     data, stats = load_dataset('data/cicids2018', 'data', '.pkl', 'stats.pkl')
     ulabels = stats[0]
+    print(stats[1])
     nfeatures = data.shape[1] - len(ulabels)
 
     # select data
 
-    labels = ['Benign', 'Brute Force -Web']
+    labels = ulabels # ['Benign', 'Brute Force -Web']
     nsamples = [1000, 100]
     X, Y = [], []
     for i in range(len(labels)):
@@ -26,4 +38,14 @@ if __name__ == '__main__':
     Y = np.vstack(Y)
     print(X.shape, Y.shape)
 
+    # test models
+
+    model_dir = 'models/dnn_{0}_{1}'
+    n_layers = [2]
+    n_hidden = [64]
+    validation_split = 0.8
+    for nl in n_layers:
+        for nh in n_hidden:
+            model = create_model(nfeatures, nl, nh, len(labels))
+            model.fit(X, Y, validation_split=validation_split, verbose=True)
 
