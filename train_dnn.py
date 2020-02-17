f3d11815d6037e8bedffa3375b0ab7ed66378876
mizolotu <38492363+mizolotu@users.noreply.github.com>
2020-02-17 11:00:29 +0200

    17.12.2019

--- train_dnn.py
+++ train_dnn.py
@@ -3,13 +3,13 @@ import numpy as np
 
 from data_proc import load_dataset
 
-def create_model(nfeatures, nlayers, nhidden, ncategories, batch=512):
+def create_model(nfeatures, nlayers, nhidden, ncategories):
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Input(shape=(nfeatures,)))
     for _ in range(nlayers):
         model.add(tf.keras.layers.Dense(nhidden, activation='relu'))
     model.add(tf.keras.layers.Dense(ncategories, activation='softmax'))
-    model.compile(loss='categorical_crossentropy')
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 if __name__ == '__main__':
@@ -36,18 +36,19 @@ if __name__ == '__main__':
             Y.append(y)
     X = np.vstack(X)
     Y = np.vstack(Y)
-    print(X.shape, Y.shape)
+    print(X.shape, Y.shape, np.sum(Y, axis=0))
 
     # test models
 
     model_dir = 'models/dnn_{0}_{1}'
     n_layers = [2]
     n_hidden = [64]
-    validation_split = 0.8
+    validation_split = 0.2
+    batch_size = 512
     epochs=10000
     for nl in n_layers:
         for nh in n_hidden:
             model = create_model(nfeatures, nl, nh, len(labels))
             model.summary()
-            model.fit(X, Y, validation_split=validation_split, epochs=epochs, verbose=True)
+            model.fit(X, Y, validation_split=validation_split, epochs=epochs, batch_size=batch_size, verbose=True)
 
