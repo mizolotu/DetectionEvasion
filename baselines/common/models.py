d2b33eccda304d930270bf39f9518bae92016f15
mizolotu@jyu.fi <mizolotu@jyu.fi>
2020-03-21 01:54:33 +0200

    20.3.2020

--- baselines/common/models.py
+++ baselines/common/models.py
@@ -79,7 +79,8 @@ def conv1d(convs=[(32, 8, 4), (64, 4, 2), (64, 3, 1)], **conv_kwargs):
                 h = tf.keras.layers.Conv1D(
                     filters=num_outputs, kernel_size=kernel_size, strides=stride,
                     activation='relu', **conv_kwargs)(h)
-                print(h)
+            h = tf.keras.layers.Flatten()(h)
+            h = tf.keras.layers.Dense(units=512, kernel_initializer=ortho_init(np.sqrt(2)), activation='relu')(h)
         network = tf.keras.Model(inputs=[x_input], outputs=[h])
         return network
     return network_fn
