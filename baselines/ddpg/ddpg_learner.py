5e7f9381e54e4b5b8e317114b9b6df6246af4ab8
mizolotu@jyu.fi <mizolotu@jyu.fi>
2020-03-25 13:28:11 +0200

    25.3.2020

--- baselines/ddpg/ddpg_learner.py
+++ baselines/ddpg/ddpg_learner.py
@@ -114,6 +114,7 @@ class DDPG(tf.Module):
         if self.critic_l2_reg > 0.:
             critic_reg_vars = []
             for layer in self.critic.network_builder.layers[1:]:
+                print(layer.name)
                 critic_reg_vars.append(layer.kernel)
             for var in critic_reg_vars:
                 logger.info('  regularizing: {}'.format(var.name))
